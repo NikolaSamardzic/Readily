281bcd248a22377190b81b49ce66d88d7de428c0
NikolaSamardzic <95717887+NikolaSamardzic@users.noreply.github.com>
2023-03-01 14:28:51 +0100

    json almost done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
-console.log("radi")
+
+window.onload = function(){
+    ajaxCallBack("assets/data/books.json","get",(result)=>{
+        
+        // var knjige = result.filter(x=>x.reviews.stars==5 && x.reviews.reviewsNumber >15000);
+        // //console.log(knjige);
+        // knjige.sort((a,b)=>{
+        //     return b.reviews.reviewsNumber - a.reviews.reviewsNumber
+        // })
+        result.sort((a,b)=>{
+            return a.reviews.reviewsNumber - b.reviews.reviewsNumber
+        })
+
+
+        console.log(result)
+    })
+}
+
+function ajaxCallBack(link,metod,result){
+    $.ajax({
+        url:link,
+        method:metod,
+        dataType:"json",
+        success:result
+    })
+}
